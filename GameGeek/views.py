d07cc58351737b0e1078356b6a0869d8048b848f
CETECOM <re.caro@duocuc.cl>
2024-05-31 12:02:43 -0400

    Migración a Django

---
+++ GameGeek/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# def ofertas(request, img_oferta):
+#     context ={
+#         "img_oferta":img_oferta
+#     }
+#     return render(request, f'base/ofertas.html', context)
