afe925e2daeac93dc29c006f38f8405c1cb83842
CETECOM <mauriciouch15@gmail.com>
2024-06-04 11:40:14 -0400

    Migración paginas 1/3

--- GameGeek/urls.py
+++ GameGeek/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('', views.index, name='index'),
+    path('pages/despacho.html', views.despacho, name='despacho'),
+    path('pages/registro.html', views.registro, name='registro'),
+    path('pages/peluches.html', views.peluches, name='peluches'),
+    path('pages/pines.html', views.pines, name='pines'),
+    path('pages/figuras.html', views.figuras, name='figuras')
 ]
--- GameGeek/views.py
+++ GameGeek/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def ofertas(request, img_oferta):
-#     context ={
-#         "img_oferta":img_oferta
-#     }
-#     return render(request, f'base/ofertas.html', context)
+def index(request):
+    return render(request, 'pages/index.html')
+
+def despacho(request):
+    return render(request, 'pages/despacho.html')
+
+def registro(request):
+    return render(request, 'pages/registro.html')
+
+def peluches(request):
+    return render(request, 'pages/peluches.html')
+
+def pines(request):
+    return render(request, 'pages/pines.html')
+
+def figuras(request):
+    return render(request, 'pages/figuras.html')
